0cf25dafd1ee813b15677a63831789037b538b42
Harvey Fletcher <harvey.fletcher@reassured.co.uk>
2018-02-12 16:59:07 +0000

    List all meetings the user has scheduled

--- API/database_changes.sql
+++ API/database_changes.sql
@@ -43,3 +43,20 @@ CREATE TABLE application_tokens(
 /*Insert a test dataset to the calendar*/
 INSERT INTO company_calendar (`event_name`,`event_location`,`event_organiser`,`event_start`,`event_end`,`event_information`) VALUES ('Test Event','Basingstoke Office','Harvey Fletcher','2018-01-26 09:00:00','2018-01-26 17:30:00','This is for test purposes');
 INSERT INTO company_calendar (`event_name`,`event_location`,`event_organiser`,`event_start`,`event_end`,`event_information`) VALUES ('Test Event 2','Portsmouth Office','Amanda Miranda Panda','2018-01-26 16:00:00','2018-01-26 17:30:00','This is for test purposes');
+
+/*This is a table that contains scheduled meetings*/
+CREATE TABLE scheduled_meetings(
+	id int(10) AUTO_INCREMENT,
+	organizer_id int(10) NOT NULL,
+	title varchar(50) NOT NULL,
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	start_time TIMESTAMP,
+	duration INT(4),
+	invited VARCHAR(2000) DEFAUlT "[]",
+	attending VARCHAR(2000) DEFAULT "[]",
+	declined VARCHAR(2000) DEFAULT "[]",
+	PRIMARY KEY(id)
+);
+
+/*Insert a test meeting*/
+insert into scheduled_meetings (`organizer_id`,`title`,`start_time`,`duration`,`invited`,`attending`,`declined`) VALUES ('1','Test Meeting','2018-02-12 17:00:00','30','[1]','[]','[]');
--- app/src/main/java/uk/co/reassured/reassuredmobileapp/Meetings.java
+++ app/src/main/java/uk/co/reassured/reassuredmobileapp/Meetings.java
@@ -6,10 +6,15 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.RelativeSizeSpan;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +23,9 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -30,8 +37,8 @@ public class Meetings extends AppCompatActivity {
     //This is the context
     private Context ctx = Meetings.this;
 
-    //This is used for storing responses
-    public JSONArray XMLResponse = new JSONArray();
+    //Used for positioning items
+    public int StaticID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,29 +53,296 @@ public class Meetings extends AppCompatActivity {
             }
         });
 
-        JSONObject PostData = new JSONObject();
+        //Make a post request to get the existing meetings.
         try{
-            PostData.put("action","test");
+            JSONObject PostData = new JSONObject();
+            PostData.put("action","ListPersonalMeetings");
+            PerformPostRequest(new OnJSONResponseCallback(){
+                @Override
+                public JSONArray onJSONResponse(boolean success, JSONArray response){
+                    DisplayExistingMeetings(response);
+                    return response;
+                }
+            }, PostData);
         } catch (Exception e){
             e.printStackTrace();
         }
-
-        getJSONObj(new OnJSONResponseCallback(){
-            @Override
-            public void onJSONResponse(boolean success, JSONArray response){
-                System.out.println("RESPONSE! \n \n " + response + " \n \n -------------------------------");
-            }
-        }, PostData);
     };
 
-    public interface OnJSONResponseCallback {
-        public void onJSONResponse(boolean success, JSONArray response);
+    public void DisplayExistingMeetings(final JSONArray Meetings){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //Get all the time frames (Today, Tomorrow, Beyond)
+                JSONObject AllTimeframes = new JSONObject();
+                System.out.println(Meetings);
+
+                try{
+                    AllTimeframes = Meetings.getJSONObject(0);
+                } catch (Exception e){
+                    Toast.makeText(ctx, "There are no meetings.", Toast.LENGTH_LONG).show();
+                }
+
+                //Get all existing meetings
+                try{
+                    //Iterate through all of today's meetings.
+                    IterateTodayMeetings(AllTimeframes);
+
+                    //Iterate through all of tomorrow's meetings.
+                    IterateTomorrowMeetings(AllTimeframes);
+
+                    //Iterate through future meetings
+                    IterateFutureMeetings(AllTimeframes);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    public JSONObject getJSONObj(final OnJSONResponseCallback callback, JSONObject PostData) {
+    public void IterateFutureMeetings(JSONObject AllTimeframes) throws JSONException{
+        //This is the container for existing meetings
+        RelativeLayout ExistingMeetingsContainer = findViewById(R.id.ExistingMeetingsContainer);
+
+        //This container says "Future"
+        RelativeLayout FutureHeader = new RelativeLayout(ctx);
+        TextView FutureHeaderText = new TextView(ctx);
+        FutureHeaderText.setText("Future:");
+        FutureHeaderText.setTextSize(15);
+        FutureHeader.addView(FutureHeaderText);
+        RelativeLayout.LayoutParams FutureHeaderParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        FutureHeaderParams.addRule(RelativeLayout.BELOW, StaticID);
+        FutureHeader.setLayoutParams(FutureHeaderParams);
+        FutureHeader.setId(StaticID + 1);
+        ExistingMeetingsContainer.addView(FutureHeader);
+        ExistingMeetingsContainer.refreshDrawableState();
+
+        //Increment staticID
+        StaticID++;
+
+        //Get all the meetings today
+        JSONArray FutureMeetings = AllTimeframes.getJSONArray("future");
+
+        //How many meetings are there.
+        int MeetingsTodayCount = FutureMeetings.length();
+
+        for(int i=0;i<MeetingsTodayCount;i++){
+            //Build up the meeting data
+            JSONObject meeting = new JSONObject(FutureMeetings.getString(i));
+            String title = meeting.getString("title");
+            String location = meeting.getString("location");
+            String start = meeting.getString("start_time").substring(meeting.getString("start_time").length() -8, meeting.getString("start_time").length());
+            int invited = new JSONArray(meeting.getString("invited")).length();
+            int attending = new JSONArray(meeting.getString("attending")).length();
+            int declined = new JSONArray(meeting.getString("declined")).length();
+
+            //The meeting goes in a spannable layout so that it can be ordered.
+            RelativeLayout IndividualMeetingContainer = new RelativeLayout(ctx);
+
+            //Give it an ID so we can assign it parameters
+            IndividualMeetingContainer.setId(StaticID + 1);
+
+            //Each meeting is in a textview
+            TextView Meeting = new TextView(ctx);
+
+            //Build a spannable string to populate the meeting details
+            SpannableString MeetingDetails = BuildMeetingSpannable(title, location, start, invited, attending, declined);
+            Meeting.setText(MeetingDetails);
+            Meeting.setX(10);
+
+            //Add the textview to the container
+            IndividualMeetingContainer.addView(Meeting);
+
+            //The container needs to be positioned below the previous one
+            RelativeLayout.LayoutParams ContainerParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            //Display this box below the previous one
+            ContainerParams.addRule(RelativeLayout.BELOW, StaticID);
+            ContainerParams.setMargins(0,0,0,20);
+
+            //Apply the parameters
+            IndividualMeetingContainer.setLayoutParams(ContainerParams);
+
+            //Add the individual meeting container so it can be seen.
+            ExistingMeetingsContainer.addView(IndividualMeetingContainer);
+
+            //Increment static ID by one So we can place future meetings below it.
+            StaticID++;
+        }
+    }
+
+    public void IterateTomorrowMeetings(JSONObject AllTimeframes) throws JSONException{
+        //This is the container for existing meetings
+        RelativeLayout ExistingMeetingsContainer = findViewById(R.id.ExistingMeetingsContainer);
+
+        //This container says "Tomorrow"
+        RelativeLayout TomorrowHeader = new RelativeLayout(ctx);
+        TextView TomorrowHeaderText = new TextView(ctx);
+        TomorrowHeaderText.setText("Tomorrow:");
+        TomorrowHeaderText.setTextSize(15);
+        TomorrowHeader.addView(TomorrowHeaderText);
+        RelativeLayout.LayoutParams TomorrowHeaderParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        TomorrowHeaderParams.addRule(RelativeLayout.BELOW, StaticID);
+        TomorrowHeader.setLayoutParams(TomorrowHeaderParams);
+        TomorrowHeader.setId(StaticID + 1);
+        ExistingMeetingsContainer.addView(TomorrowHeader);
+        ExistingMeetingsContainer.refreshDrawableState();
+
+        //Increment staticID
+        StaticID++;
+
+        //Get all the meetings today
+        JSONArray TomorrowMeetings = AllTimeframes.getJSONArray("tomorrow");
+
+        //How many meetings are there.
+        int MeetingsTodayCount = TomorrowMeetings.length();
+
+        for(int i=0;i<MeetingsTodayCount;i++){
+            //Build up the meeting data
+            JSONObject meeting = new JSONObject(TomorrowMeetings.getString(i));
+            String title = meeting.getString("title");
+            String location = meeting.getString("location");
+            String start = meeting.getString("start_time").substring(meeting.getString("start_time").length() -8, meeting.getString("start_time").length());
+            int invited = new JSONArray(meeting.getString("invited")).length();
+            int attending = new JSONArray(meeting.getString("attending")).length();
+            int declined = new JSONArray(meeting.getString("declined")).length();
+
+            //The meeting goes in a spannable layout so that it can be ordered.
+            RelativeLayout IndividualMeetingContainer = new RelativeLayout(ctx);
+
+            //Give it an ID so we can assign it parameters
+            IndividualMeetingContainer.setId(StaticID + 1);
+
+            //Each meeting is in a textview
+            TextView Meeting = new TextView(ctx);
+
+            //Build a spannable string to populate the meeting details
+            SpannableString MeetingDetails = BuildMeetingSpannable(title, location, start, invited, attending, declined);
+            Meeting.setText(MeetingDetails);
+            Meeting.setX(10);
+
+            //Add the textview to the container
+            IndividualMeetingContainer.addView(Meeting);
+
+            //The container needs to be positioned below the previous one
+            RelativeLayout.LayoutParams ContainerParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            //Display this box below the previous one
+            ContainerParams.addRule(RelativeLayout.BELOW, StaticID);
+            ContainerParams.setMargins(0,0,0,20);
+
+            //Apply the parameters
+            IndividualMeetingContainer.setLayoutParams(ContainerParams);
+
+            //Add the individual meeting container so it can be seen.
+            ExistingMeetingsContainer.addView(IndividualMeetingContainer);
+
+            //Increment static ID by one So we can place future meetings below it.
+            StaticID++;
+        }
+    }
+
+    public void IterateTodayMeetings(JSONObject AllTimeframes) throws JSONException{
+        //This is the container for existing meetings
+        RelativeLayout ExistingMeetingsContainer = findViewById(R.id.ExistingMeetingsContainer);
+
+        //This container says "Today"
+        RelativeLayout TodayHeader = new RelativeLayout(ctx);
+        TextView TodayHeaderText = new TextView(ctx);
+        TodayHeaderText.setText("Today:");
+        TodayHeaderText.setTextSize(15);
+        TodayHeader.addView(TodayHeaderText);
+        TodayHeader.setId(StaticID);
+        ExistingMeetingsContainer.addView(TodayHeader);
+        ExistingMeetingsContainer.refreshDrawableState();
+
+        //Get all the meetings today
+        JSONArray TodayMeetings = AllTimeframes.getJSONArray("today");
+
+        //How many meetings are there.
+        int MeetingsTodayCount = TodayMeetings.length();
+        String CountMessage = "";
+        if(MeetingsTodayCount > 1){
+            CountMessage = "You have " + MeetingsTodayCount + " meetings today.";
+        } else {
+            CountMessage = "You have " + MeetingsTodayCount + " meeting today.";
+        }
+        Toast.makeText(ctx, CountMessage, Toast.LENGTH_LONG).show();
+
+        for(int i=0;i<MeetingsTodayCount;i++){
+            //Build up the meeting data
+            JSONObject meeting = new JSONObject(TodayMeetings.getString(i));
+            String title = meeting.getString("title");
+            String location = meeting.getString("location");
+            String start = meeting.getString("start_time").substring(meeting.getString("start_time").length() -8, meeting.getString("start_time").length());
+            int invited = new JSONArray(meeting.getString("invited")).length();
+            int attending = new JSONArray(meeting.getString("attending")).length();
+            int declined = new JSONArray(meeting.getString("declined")).length();
+
+            //The meeting goes in a spannable layout so that it can be ordered.
+            RelativeLayout IndividualMeetingContainer = new RelativeLayout(ctx);
+
+            //Give it an ID so we can assign it parameters
+            IndividualMeetingContainer.setId(StaticID + 1);
+
+            //Each meeting is in a textview
+            TextView Meeting = new TextView(ctx);
+
+            //Build a spannable string to populate the meeting details
+            SpannableString MeetingDetails = BuildMeetingSpannable(title, location, start, invited, attending, declined);
+            Meeting.setText(MeetingDetails);
+            Meeting.setX(10);
+
+            //Add the textview to the container
+            IndividualMeetingContainer.addView(Meeting);
+
+            //The container needs to be positioned below the previous one
+            RelativeLayout.LayoutParams ContainerParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            //Display this box below the previous one
+            ContainerParams.addRule(RelativeLayout.BELOW, StaticID);
+            ContainerParams.setMargins(0,0,0,20);
+
+            //Apply the parameters
+            IndividualMeetingContainer.setLayoutParams(ContainerParams);
+
+            //Add the individual meeting container so it can be seen.
+            ExistingMeetingsContainer.addView(IndividualMeetingContainer);
+
+            //Increment static ID by one So we can place future meetings below it.
+            StaticID++;
+        }
+    }
+
+    public SpannableString BuildMeetingSpannable(String title, String location, String start, int invited, int attending, int declined){
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+
+        //Add the meeting title
+        String MeetingTitle = new String(title + "\n");
+
+        //Add the meeting location
+        String MeetingLocation = new String(location + " @ " + start.substring(0,5) + "\n");
+
+        //Add the number of people attending out of those invited and those who declined
+        String AttendingDetails = new String(attending + "/" + invited + " attending." + "\n" + declined + " declined");
+
+        SpannableString MeetingDetails = new SpannableString(MeetingTitle + MeetingLocation + AttendingDetails);
+        MeetingDetails.setSpan(new RelativeSizeSpan(2f),0, MeetingTitle.length(), 0);
+        MeetingDetails.setSpan(new RelativeSizeSpan(1.3f), MeetingTitle.length(), MeetingTitle.length() + MeetingLocation.length(), 0);
+
+        return MeetingDetails;
+    }
+
+    public interface OnJSONResponseCallback {
+        public JSONArray onJSONResponse(boolean success, JSONArray response);
+    }
+
+    public void PerformPostRequest(final OnJSONResponseCallback callback, JSONObject PostData) {
         //To authenticate against the API we need the user's credentials
         String Email = getSharedPreferences(ctx).getString("Email","");
         String Password = getSharedPreferences(ctx).getString("Password","");
+
+        final JSONArray[] ResponseStorage = new JSONArray[1];
 
         //Add the credentials to post data
         try{
@@ -106,8 +380,6 @@ public class Meetings extends AppCompatActivity {
                 }
             }
         });
-
-        return null;
     }
 
     public static SharedPreferences getSharedPreferences(Context ctx){
