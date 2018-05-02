38fd401efd1db01bc1c94a107455ba5de4ecde8b
harvey-fletcher <harvey.fletcher1@ntlworld.com>
2018-05-02 12:21:32 +0100

    Created a ClassGlobals file

---
+++ app/src/main/java/uk/co/reassured/reassuredmobileapp/ClassGlobals.java
@@ -0,0 +1,10 @@
+package uk.co.reassured.reassuredmobileapp;
+
+/**
+ * Created by hfletcher on 02/05/2018.
+ */
+
+public class ClassGlobals {
+
+    public String ApiHost = "http://rmobileapp.co.uk/";
+}
